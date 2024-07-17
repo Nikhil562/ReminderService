c6cf076fa4b0f8c97152bc54fd7cabf27b4ef9a1
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-07-17 13:53:24 +0530

    Added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { sendBasicEmail } = require('./services/email-service');
+// const { sendBasicEmail } = require('./services/email-service');
+
+const cron = require('node-cron');
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = () => {
@@ -11,12 +13,15 @@ const setupAndStartServer = () => {
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
 
-        sendBasicEmail(
-            'support@admin.com',
-            'moviebookingappservice@gmail.com',
-            'This is a testing email',
-            'Hey, how are you, I hope you like the support'
-        );
+        // sendBasicEmail(
+        //     'support@admin.com',
+        //     'moviebookingappservice@gmail.com',
+        //     'This is a testing email',
+        //     'Hey, how are you, I hope you like the support'
+        // );
+        cron.schedule('*/2 * * * *', () => {
+            console.log('running a task every two minutes');
+          });     
     });
 }
 
