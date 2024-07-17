85255c43c1470e941e5568f44416ea15bc92b072
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-07-17 13:58:23 +0530

    Added notification cron

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 // const { sendBasicEmail } = require('./services/email-service');
+const TicketController = require('./controllers/ticket-controller');
 
-const cron = require('node-cron');
+// const cron = require('node-cron');
+const jobs = require('./utils/job');
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = () => {
@@ -10,9 +12,12 @@ const setupAndStartServer = () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/tickets', TicketController.create);
+
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
 
+        jobs();
         // sendBasicEmail(
         //     'support@admin.com',
         //     'moviebookingappservice@gmail.com',
